3830f88fbadf82f10f2b54db3e699b05784bb871
Will Walton <will@endless3dge.com>
2024-04-16 12:03:10 -0500

    Update List.js
    added table tags

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,39 @@ import React from 'react'
 function List({ employees, handleEdit, handleDelete }) {
   return (
     <div>
-      List
+      <table>
+  <thead>
+    <tr>
+      <th>Head 1</th>
+      <th>Head 2</th>
+      <th>Head 3</th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <th>Footer 1</th>
+      <th>Footer 2</th>
+      <th>Footer 3</th>
+    </tr>
+  </tfoot>
+  <tbody>
+    <tr>
+      <td>Description 1</td>
+      <td>Description 2</td>
+      <td>Description 3</td>
+    </tr>
+    <tr>
+      <td>Description 1</td>
+      <td>Description 2</td>
+      <td>Description 3</td>
+    </tr>
+    <tr>
+      <td>Description 1</td>
+      <td>Description 2</td>
+      <td>Description 3</td>
+    </tr>
+  </tbody>
+</table>
     </div>
   )
 }
