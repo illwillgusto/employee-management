9f88144b67a57eb02105a7fc67c8a2cbadc12df3
Will Walton <will@endless3dge.com>
2024-04-16 11:26:14 -0500

    Update Header.js
    style for the margin added for the button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header({ setIsAdding }) {
   return (
     <header>
       <h1>Employee Management System</h1>
-    <div>
+    <div style={{ marginTop: '30px', marginBottom: '18px' }}>
       <button onClick={() => setIsAdding(true)} className='round-button'>Add Button</button>
     </div>
     </header>
