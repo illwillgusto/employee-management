c46d9f6cc3a0ef9f848e711e02314c8f6dff784a
Will Walton <will@endless3dge.com>
2024-04-16 10:11:46 -0500

    Update index.js
    imported the components
    Header
    List
    Add
    Edit

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import Swal from 'sweetalert2';
+
+import Header from './Header';
+import List from './List';
+import Add from './Add';
+import Edit from './Edit';
 
 import { employeesData } from '../../src/data';
 
