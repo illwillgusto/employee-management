b3400e97b0ef36acce1d30090d7cced475da11ce
Will Walton <will@endless3dge.com>
2024-04-16 17:29:17 -0500

    Update Edit.js
    form inputs and labels added with tag props and attributes

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -13,8 +13,60 @@ function Edit({ employees, selectedEmployee, setEmployees, setIsEditing }) {
 
 
   return (
-    <div>
-      
+    <div className='small-container'>
+      <form onSubmit={handleUpdate}>
+                <h1>Edit Employee</h1>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                    id="firstName"
+                    type="text"
+                    name="firstName"
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                />
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                    id="lastName"
+                    type="text"
+                    name="lastName"
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)}
+                />
+                <label htmlFor="email">Email</label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <label htmlFor="salary">Salary ($)</label>
+                <input
+                    id="salary"
+                    type="number"
+                    name="salary"
+                    value={salary}
+                    onChange={e => setSalary(e.target.value)}
+                />
+                <label htmlFor="date">Date</label>
+                <input
+                    id="date"
+                    type="date"
+                    name="date"
+                    value={date}
+                    onChange={e => setDate(e.target.value)}
+                />
+                <div style={{ marginTop: '30px' }}>
+                    <input type="submit" value="Update" />
+                    <input
+                        style={{ marginLeft: '12px' }}
+                        className="muted-button"
+                        type="button"
+                        value="Cancel"
+                        onClick={() => setIsEditing(false)}
+                    />
+                </div>
+            </form>
     </div>
   )
 }
