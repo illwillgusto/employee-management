0d5df81e9ed5397bd591a64f68891a2cdeb1a5a7
Will Walton <will@endless3dge.com>
2024-04-16 11:23:38 -0500

    Update Header.js
    added a Add Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Header({ setIsAdding }) {
   return (
+    <header>
+      <h1>Employee Management System</h1>
     <div>
-      
+      <button>Add Button</button>
     </div>
+    </header>
   )
 }
 
