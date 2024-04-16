8c4817a962e4ce7b618909606a7197ae69db5687
Will Walton <will@endless3dge.com>
2024-04-16 14:40:27 -0500

    Update List.js
    props added for the table headers
    the table data for each column
    
    added the edit and delete buttons for action
    
    removed the footer section as well

--- src/components/List.js
+++ src/components/List.js
@@ -15,32 +15,32 @@ function List({ employees, handleEdit, handleDelete }) {
       <th colSpan={2} className='text-center'>Actions</th>
     </tr>
   </thead>
-  <tfoot>
-    <tr>
-      <th>Footer 1</th>
-      <th>Footer 2</th>
-      <th>Footer 3</th>
-    </tr>
-  </tfoot>
   <tbody>
-    <tr>
-      <td>Description 1</td>
-      <td>Description 2</td>
-      <td>Description 3</td>
-    </tr>
-    <tr>
-      <td>Description 1</td>
-      <td>Description 2</td>
-      <td>Description 3</td>
-    </tr>
-    <tr>
-      <td>Description 1</td>
-      <td>Description 2</td>
-      <td>Description 3</td>
-    </tr>
-  </tbody>
-</table>
-    </div>
+    {employees.length > 0 ? (
+      employees.map((employee, i) => (
+        <tr key={employee.id}>
+          <td>{i + 1}</td>
+          <td>{employee.firstName}</td>
+          <td>{employee.lastName}</td>
+          <td>{employee.email}</td>
+          <td>{formatter.format(employee.salary)}</td>
+          <td>{employee.startDate} </td>
+          <td className='text-right'>
+            <button onClick={() => handleEdit(employee.id)} className='"button muted-button'>Edit</button>
+          </td>
+          <td className='text-left'>
+            <button onClick={() => handleDelete(employee.id)} className='"button muted-button'>Delete</button>
+          </td>
+        </tr>
+      ))
+    ) : (
+        <tr>
+         <td colSpan={7}>No Employees</td>
+        </tr>
+    )}
+     </tbody>
+   </table>
+  </div>
   )
 }
 
