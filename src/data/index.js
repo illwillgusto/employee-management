f9bb240c217000d8b3a59d23d71906467e4b3431
Will Walton <will@endless3dge.com>
2024-04-16 14:52:27 -0500

    Update index.js
    changed the prop string for the the employee data from date to startDate

--- src/data/index.js
+++ src/data/index.js
@@ -5,7 +5,7 @@ const employeesData = [
         lastName: 'Doe',
         email: 'johndoe@example.com',
         salary: '9500',
-        date: '2024-04-16'
+        startDate: '2024-04-16'
     },
 ];
 
