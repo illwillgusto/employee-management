e81bfc1626c26c6ba5e47f3d478526554a293b77
Will Walton <will@endless3dge.com>
2024-04-19 13:38:53 -0500

    Update index.js
    corrected the path to where the frontend should be fetching the data

--- src/components/index.js
+++ src/components/index.js
@@ -16,7 +16,7 @@ function Dashboard() {
     const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
-        fetch('/employees')
+        fetch('http://localhost:3003/employees')
           .then(response => response.json())
           .then(data => {
             console.log("Data received from backend:", data);
