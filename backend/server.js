cc4d96864f591ce7b10a079cd73ebc0457d2491f
Will Walton <will@endless3dge.com>
2024-04-25 18:58:50 -0500

    Update server.js
    origin port changed

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,9 @@ const DATA_FILE = './employeesData.json';
 
 // this tells the backend what ports are allowed to fetch data from the json file
 app.use(cors({
-    origin: 'http://localhost:3002',
-    methods: ['GET', 'POST']
+    origin: 'http://localhost:3005',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
