cf84938f83a5df910858cbcab3e0b73c48bfe8cd
Will Walton <will@endless3dge.com>
2024-04-25 07:16:23 -0500

    Update server.js
    backend port changed
    removed the delete function
    removed the write the updated array after user is deleted function and if statement as well

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3003;
 const DATA_FILE = './employeesData.json';
 
 app.use(cors({
-    origin: 'http://localhost:3001'
+    origin: 'http://localhost:3003'
 }));
 app.use(bodyParser.json());
 
@@ -23,7 +23,7 @@ app.get('/employees', (req, res) => {
     });
 });
 
-// Endpoint to add or update employees 
+// Endpoint to add or update employees
 app.post('/employees', (req, res) => {
     const newEmployee = req.body;
     fs.readFile(DATA_FILE, (err, data) => {
@@ -39,28 +39,6 @@ app.post('/employees', (req, res) => {
                 return;
             }
             res.send('Employee added/updated.');
-        });
-    });
-});
-
-app.delete('/employees/:id', (req, res) => {
-    const employeeId = parseInt(req.params.id);
-
-    fs.readFile(DATA_FILE, (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading data file.');
-        }
-
-        // Remove the employee
-        employees.splice(index, 1);
-
-        // Write the updated array back to the file 
-        fs.writeFile(DATA_FILE, JSON.stringify(employees, null, 2), (err) => {
-            if (err) {
-                res.status(500).send('Error writing to data file.');
-                return;
-            }
-            res.status(204).send(); // no content to send back 
         });
     });
 });
